7173cf050082ab6f3f8bd9b01fc7dbd5f33fe37b
Laura Berge <lauraeberge@gmail.com>
2019-09-20 17:55:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,6 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   FROM pledges 
   INNER JOIN projects
   ON projects.id = pledges.project_id
+  GROUP BY pledges.project_id
   ORDER BY projects.title;"
 end
 
