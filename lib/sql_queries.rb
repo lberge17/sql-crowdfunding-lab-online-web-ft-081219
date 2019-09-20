8a6224d5d5af21d2529f4b92454e42537085acf7
Laura Berge <lauraeberge@gmail.com>
2019-09-20 18:11:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, pledges.amount
   FROM pledges
   INNER JOIN projects
-  ON projects.category = 'music' AND projects.id = pledges.project_id;"
+  ON projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
