4a1fddd78e020c8827b3d401bc6f7e0347c02836
Laura Berge <lauraeberge@gmail.com>
2019-09-20 17:56:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,11 +21,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   INNER JOIN users
   ON users.id = pledges.user_id
   GROUP BY pledges.user_id
-  ORDER BY users.name"
+  ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT projects.title, SUM(project."
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
